2200cb44d4f512c1d2d58d25758dfa90eec78ef0
MatheusBruno <matheusbrunosilveira@gmail.com>
2023-07-20 12:43:03 -0300

    .editado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = document.getElementById("app")
 const arrumar = document.getElementById("arrumar")
 
-function dados2() {
+async function dados2() {
     return fetch('https://brapi.dev/api/quote/AMER3%2CPETR4%2CKLBN11%2CITSA4%2CBRSR6%2CCSNA3%2CTAEE4%2CTASA4?range=1d&interval=1d&fundamental=true&dividends=true').then(resultado => resultado.json()).then(resultado => resultado['results'])
 }
 
@@ -12,18 +12,19 @@ function htmlcompleto(dados){
     <h1>${dados.longName}</h1>
     <h2>${dados.symbol}</h2>
     <p>R$ ${dados.regularMarketPrice}</p>
-    ${ dados.dividendsData.cashDividends.map(resultado => {
+    ${ dados.dividendsData.cashDividends ? 
+        dados.dividendsData.cashDividends.map(resultado => {
         
-        return `<div class="dividendos">
-        <h3>${resultado.label}</h3>
-        <p>R$ ${resultado.rate.toFixed(2)}</p>
-        <p>Data de Pagamento: ${resultado.paymentDate.replace("T00:00:00.000Z","")}</p>
-        <p>Ultimo dia par entrar: ${resultado.lastDatePrior.replace("T00:00:00.000Z","")}</p>
-        </div>`
-
-
-    }).join('') }
-
+            return `<div class="dividendos">
+            <h3>${resultado.label}</h3>
+            <p>R$ ${resultado.rate.toFixed(2)}</p>
+            <p>Data de Pagamento: ${resultado.paymentDate.replace("T00:00:00.000Z","")}</p>
+            <p>Ultimo dia par entrar: ${resultado.lastDatePrior.replace("T00:00:00.000Z","")}</p>
+            </div>`
+    
+    
+        }).join('')
+        : "<p> Não Disponivel no momento </p>"}
     </div>
     `
 }
